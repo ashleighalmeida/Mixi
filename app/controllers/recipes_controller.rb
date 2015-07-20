001327ecde843e5fb49b82f8bcc61e165f66226e
Ashleigh <ashleighalmeida@gmail.com>
2015-07-20 12:18:32 -0400

    update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,49 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @precipe = Recipe.new
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_url
+    else
+      render:new
+  end
+end
+
+def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update_attributes(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :instructions, :ingredients)
+  end
+
 end
